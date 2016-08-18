c6b38c4326c40d5c11b1223246ce247e42e3cf37
jonwhitey <jonathan42white@gmail.com>
2016-08-18 14:53:53 -0500

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: '400',
+            suffix: "_small_1x",
+            quality: 15,
+          },{
+            width: '800',
+            suffix: "_medium_1x",
+            quality: 20,
+          },{
+            width: '1600',
+            suffix: "_large_2x",
+            quality: 60,
+          }
+          ]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
+    pagespeed: {
+      options: {
+        nokey: true,
+        url: "https://developers.google.com"
+      },
+      prod: {
+        options: {
+          url: "https://developers.google.com/speed/docs/insights/v1/getting_started",
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 80
+        }
+      },
+      paths: {
+        options: {
+          paths: ["/speed/docs/insights/v1/getting_started", "/speed/docs/about"],
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 80
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.loadNpmTasks('grunt-pagespeed')
+};
